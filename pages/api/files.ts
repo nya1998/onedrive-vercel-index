932a1639b219346cef122cda90ea852692653b21
Nya1998 <ryn.nya1998@gmail.com>
2023-02-08 11:50:34 +0700

    Refer check

--- pages/api/files.ts
+++ pages/api/files.ts
@@ -7,6 +7,7 @@ import Cors from 'cors'
 import CryptoJS from 'crypto-js'
 import { driveApi, cacheControlHeader } from '../../config/api.config'
 import { encodePath, getAccessToken, checkAuthRoute } from '.'
+import e from 'cors'
 
 const AES_SECRET_KEY = 'KwOurxh6Lo'
 export function decryptPayload(obfuscated: string): string {
@@ -47,6 +48,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const odpt = 'c202498aea9cd692709d37368b7b617ea30c147e5f6b2bc32d5a742aac85d717'
 
   const x = decryptPayload(payload as string)
+  const refer = req.headers['referer']
+  const shortLink = ['khaddavi.net']
 
   // Sometimes the path parameter is defaulted to '[...path]' which we need to handle
   if (payload === '[...path]') {
@@ -75,37 +78,46 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     res.setHeader('Cache-Control', 'no-cache')
   }
 
-  await runCorsMiddleware(req, res)
-  try {
-    // Handle response from OneDrive API
-    const requestUrl = `${driveApi}/root${encodePath(cleanPath)}`
-    const { data } = await axios.get(requestUrl, {
-      headers: { Authorization: `Bearer ${accessToken}` },
-      params: {
-        // OneDrive international version fails when only selecting the downloadUrl (what a stupid bug)
-        select: 'id,size,@microsoft.graph.downloadUrl',
-      },
-    })
+  //Referer check and empty payload handling
+  if (!shortLink.includes(refer)) {
+    res.status(403).json({ error: 'Sepertinya anda bukan dari beruanglaut. Hanya download dari beruanglaut, bukan yang lain. Jika sudah, coba ganti browser yang anda gunakan.' })
+    return
+  } else if(x == ""){
+    res.status(400).json({ error: 'Invalid Payload' })
+    return
+  } else {
+    await runCorsMiddleware(req, res)
+    try {
+      // Handle response from OneDrive API
+      const requestUrl = `${driveApi}/root${encodePath(cleanPath)}`
+      const { data } = await axios.get(requestUrl, {
+        headers: { Authorization: `Bearer ${accessToken}` },
+        params: {
+          // OneDrive international version fails when only selecting the downloadUrl (what a stupid bug)
+          select: 'id,size,@microsoft.graph.downloadUrl',
+        },
+      })
 
-    if ('@microsoft.graph.downloadUrl' in data) {
-      // Only proxy raw file content response for files up to 4MB
-      if (proxy && 'size' in data && data['size'] < 4194304) {
-        const { headers, data: stream } = await axios.get(data['@microsoft.graph.downloadUrl'] as string, {
-          responseType: 'stream',
-        })
-        headers['Cache-Control'] = cacheControlHeader
-        // Send data stream as response
-        res.writeHead(200, headers)
-        stream.pipe(res)
+      if ('@microsoft.graph.downloadUrl' in data) {
+        // Only proxy raw file content response for files up to 4MB
+        if (proxy && 'size' in data && data['size'] < 4194304) {
+          const { headers, data: stream } = await axios.get(data['@microsoft.graph.downloadUrl'] as string, {
+            responseType: 'stream',
+          })
+          headers['Cache-Control'] = cacheControlHeader
+          // Send data stream as response
+          res.writeHead(200, headers)
+          stream.pipe(res)
+        } else {
+          res.redirect(data['@microsoft.graph.downloadUrl'])
+        }
       } else {
-        res.redirect(data['@microsoft.graph.downloadUrl'])
+        res.status(404).json({ error: 'File Not Found.', referer: x })
       }
-    } else {
-      res.status(404).json({ error: 'Files Not Found.' })
+      return
+    } catch (error: any) {
+      res.status(error?.response?.status ?? 500).json({ error: error?.response?.data ?? 'Internal server error.' })
+      return
     }
-    return
-  } catch (error: any) {
-    res.status(error?.response?.status ?? 500).json({ error: error?.response?.data ?? 'Internal server error.' })
-    return
   }
 }
