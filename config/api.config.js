b679320eafdfeb85cc3869cb075fc496aa7cf58f
Nya1998 <ryn.nya1998@gmail.com>
2025-02-11 21:54:26 +0700

    config: move oauth to env (2)

--- config/api.config.js
+++ config/api.config.js
@@ -10,8 +10,8 @@
 module.exports = {
   // The clientId and clientSecret are used to authenticate the user with Microsoft Graph API using OAuth. You would
   // not need to change anything here if you can authenticate with your personal Microsoft account with OneDrive International.
-  clientId: process.env.AUTH_CLIENT || 'null',
-  obfuscatedClientSecret: process.env.AUTH_KEY || 'null',
+  clientId: process.env.AUTH_CLIENT || 'aece2cb8-3f41-44d2-9818-9c3d375a0260',
+  obfuscatedClientSecret: process.env.AUTH_KEY || 'U2FsdGVkX1/JfxvycCRftPvY5INlucoh0N1C7m36Y3xIE3QwdNkMfwidzNY7qQWJJmdhbYZaI11eitOkoVCkHw==',
 
   // The redirectUri is the URL that the user will be redirected to after they have authenticated with Microsoft Graph API.
   // Likewise, you would not need to change redirectUri if you are using your personal Microsoft account with OneDrive International.
